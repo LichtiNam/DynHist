528d39c58ffea30b98d2f16bd19acdfc7dc9a3f3
LichtiNam <goerickm@zedat.fu-berlin.de>
2015-01-18 18:01:23 +0100

    interface for dynamic histogram

--- src/main/java/de/fuberlin/dynhist/Histogram.java
+++ src/main/java/de/fuberlin/dynhist/Histogram.java
@@ -1,7 +1,25 @@
 package de.fuberlin.dynhist;
 
 /**
- * Created by lichtbringer on 18.01.15.
+ * @author Martin Görick
+ *
+ * Interface to operate with histograms
  */
 public interface Histogram {
+
+  /**
+   * add a double to the histogram.
+   * @param input
+   */
+  public void addInput(double input);
+
+  /**
+   * Returns the histogram. Have to be cast.
+   */
+  public Object getHistogram();
+
+  /**
+   * write histogram if to file if buckets serializable
+   */
+  public void writeHist();
 }
