4ae32ce31374b5becda53e904355c1484307073c
LchtNam <goerickm@inf.fu-berlin.de>
2015-02-26 21:50:28 +0100

    adjust test because of repartition and rename

--- src/test/java/de/fuberlin/dynhist/CompressedHistogramImplTest.java
+++ src/test/java/de/fuberlin/dynhist/CompressedHistogramImplTest.java
@@ -8,34 +8,33 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Created by Martin Görick
+ * @author Martin Görick
  */
 public class CompressedHistogramImplTest {
 
   private double[] input;
-  private int buckets;
-  private Histogram histogram;
+  private CompressedHistogram histogram;
 
   @Before
   public void setUp() {
     input = new double[]{1.4,1.5,1.2,1.2,2.2,4.1,5.5,3.6,2.7,2.8,6,7.2};
-    buckets = 6;
-    histogram = new CompressedHistogram(buckets, 1);
+    int buckets = 6;
+    histogram = new CompressedHistogramImpl(buckets, 1);
   }
 
   @Test
   public void histogramTest() {
     for(double d : input) {
-      histogram.addInput(d);
+      histogram.addValue(d);
     }
 
     List<Bucket> hist = histogram.getHistogram();
     assertEquals(1.2, hist.get(0).getLeftBorder(), 0.1);
-    assertEquals(1.4, hist.get(1).getLeftBorder(), 0.1);
-    assertEquals(1.6, hist.get(2).getLeftBorder(), 0.1);
-    assertEquals(3.0, hist.get(3).getLeftBorder(), 0.1);
-    assertEquals(3.96, hist.get(4).getLeftBorder(), 0.1);
-    assertEquals(5.36, hist.get(5).getLeftBorder(), 0.1);
+    assertEquals(1.23, hist.get(1).getLeftBorder(), 0.1);
+    assertEquals(1.43, hist.get(2).getLeftBorder(), 0.1);
+    assertEquals(1.61, hist.get(3).getLeftBorder(), 0.1);
+    assertEquals(2.57, hist.get(4).getLeftBorder(), 0.1);
+    assertEquals(2.8, hist.get(5).getLeftBorder(), 0.1);
 
     for (Bucket bucket : hist) {
       assertEquals(2 , bucket.getCount());
