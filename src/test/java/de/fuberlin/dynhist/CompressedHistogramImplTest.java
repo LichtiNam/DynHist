ee6a1730380b4d62122492427d2448f2a4ba7b33
LchtNam <goerickm@inf.fu-berlin.de>
2015-02-27 18:40:16 +0100

    remove partition

--- src/test/java/de/fuberlin/dynhist/CompressedHistogramImplTest.java
+++ src/test/java/de/fuberlin/dynhist/CompressedHistogramImplTest.java
@@ -19,7 +19,7 @@ public class CompressedHistogramImplTest {
   public void setUp() {
     input = new double[]{1.4,1.5,1.2,1.2,2.2,4.1,5.5,3.6,2.7,2.8,6,7.2};
     int buckets = 6;
-    histogram = new CompressedHistogramImpl(buckets, 1);
+    histogram = new CompressedHistogramImpl(buckets);
   }
 
   @Test
@@ -30,14 +30,17 @@ public class CompressedHistogramImplTest {
 
     List<Bucket> hist = histogram.getHistogram();
     assertEquals(1.2, hist.get(0).getLeftBorder(), 0.1);
-    assertEquals(1.23, hist.get(1).getLeftBorder(), 0.1);
+    assertEquals(1.4, hist.get(1).getLeftBorder(), 0.1);
     assertEquals(1.43, hist.get(2).getLeftBorder(), 0.1);
-    assertEquals(1.61, hist.get(3).getLeftBorder(), 0.1);
-    assertEquals(2.57, hist.get(4).getLeftBorder(), 0.1);
-    assertEquals(2.8, hist.get(5).getLeftBorder(), 0.1);
+    assertEquals(2.2, hist.get(3).getLeftBorder(), 0.1);
+    assertEquals(4.1, hist.get(4).getLeftBorder(), 0.1);
+    assertEquals(5.5, hist.get(5).getLeftBorder(), 0.1);
 
-    for (Bucket bucket : hist) {
-      assertEquals(2 , bucket.getCount());
-    }
+    assertEquals(2 , hist.get(0).getCount());
+    assertEquals(1 , hist.get(1).getCount());
+    assertEquals(1 , hist.get(2).getCount());
+    assertEquals(4 , hist.get(3).getCount());
+    assertEquals(1 , hist.get(4).getCount());
+    assertEquals(3 , hist.get(5).getCount());
   }
 }
